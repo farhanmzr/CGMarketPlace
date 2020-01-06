60a3049a7aeef40bab65110cb1dd568c6702ec78
Farhan Mazario <farhanrio2805@gmail.com>
2020-01-06 23:39:43 +0700

    add splash,landing page activity

---
+++ app/src/main/java/com/example/cgmarketplace/activities/LandingPage1Activity.java
@@ -0,0 +1,16 @@
+package com.example.cgmarketplace.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cgmarketplace.R;
+
+public class LandingPage1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page1);
+    }
+}
