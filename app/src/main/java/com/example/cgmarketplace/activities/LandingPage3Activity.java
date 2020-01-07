cb3ae618e6710e7939a04045eb52c36c089eec42
Farhan Mazario <farhanrio2805@gmail.com>
2020-01-07 19:00:58 +0700

    90% clear login

--- app/src/main/java/com/example/cgmarketplace/activities/LandingPage3Activity.java
+++ app/src/main/java/com/example/cgmarketplace/activities/LandingPage3Activity.java
@@ -2,15 +2,32 @@ package com.example.cgmarketplace.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cgmarketplace.R;
 
 public class LandingPage3Activity extends AppCompatActivity {
 
+    Button btn_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page3);
+
+        btn_login=findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotologin = new Intent(LandingPage3Activity.this, LoginActivity.class);
+                startActivity(gotologin);
+                finish();
+                overridePendingTransition(0,0);
+                getIntent().addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            }
+        });
     }
 }
