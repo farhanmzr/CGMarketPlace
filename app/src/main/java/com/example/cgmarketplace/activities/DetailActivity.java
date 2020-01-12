375a920ef84c2e151beaeeb677ac377c15929ffa
Farhan Mazario <farhanrio2805@gmail.com>
2020-01-12 23:43:13 +0700

    fixing detail (view pager,indicator,layout)

--- app/src/main/java/com/example/cgmarketplace/activities/DetailActivity.java
+++ app/src/main/java/com/example/cgmarketplace/activities/DetailActivity.java
@@ -2,20 +2,29 @@ package com.example.cgmarketplace.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.cgmarketplace.R;
 
-import me.relex.circleindicator.CircleIndicator3;
+import com.example.cgmarketplace.R;
+import com.example.cgmarketplace.adapter.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+
+
 
 public class DetailActivity extends AppCompatActivity {
     public static final String KEY_RESTAURANT_ID = "key_restaurant_id";
+
+    ViewPager viewPager;
+    PagerAdapter adapter;
+    int[] img;
 
     ImageView btn_back;
     TextView tv_nama, tv_price, tv_desc, tv_width, tv_height, tv_dense, tv_finishing, tv_material, tv_details_1, tv_details_2, tv_details_3, tv_details_4;
@@ -24,6 +33,16 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        img=new int[]{R.drawable.img_sofa, R.drawable.img_sofa2, R.drawable.img_sofa3};
+        //view pager code
+        viewPager=(ViewPager) findViewById(R.id.pager);
+        adapter=new ViewPagerAdapter(DetailActivity.this, img);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager, true);
+
 
         btn_back = findViewById(R.id.btn_back);
         tv_nama = findViewById(R.id.tv_nama);
---
+++ app/src/main/java/com/example/cgmarketplace/adapter/ViewPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.example.cgmarketplace.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.cgmarketplace.R;
+import com.example.cgmarketplace.activities.DetailActivity;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    int[] image;
+    LayoutInflater inflater;
+    Context context;
+    int position =3;
+
+    public ViewPagerAdapter(DetailActivity detailActivity,int[] img)
+    {
+        this.context=detailActivity;
+        this.image=img;
+    }
+
+    @Override
+    public int getCount() {
+        return image.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==((RelativeLayout)object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        ImageView img_pager;
+        inflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemview=inflater.inflate(R.layout.item_pager,container,false);
+        img_pager=(ImageView) itemview.findViewById(R.id.img_pager);
+        img_pager.setImageResource(image[position]);
+
+        //add item_pager to ViewPager
+        ((ViewPager)container).addView(itemview);
+        return itemview;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ((ViewPager)container).removeView((RelativeLayout)object);
+    }
+
+}
