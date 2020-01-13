61d175c949614ba19846fa3cd5e071142dd60d85
Farhan Mazario <farhanrio2805@gmail.com>
2020-01-13 18:15:42 +0700

    function btn + - cart

--- app/src/main/java/com/example/cgmarketplace/activities/CartActivity.java
+++ app/src/main/java/com/example/cgmarketplace/activities/CartActivity.java
@@ -6,16 +6,68 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.cgmarketplace.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CartActivity extends AppCompatActivity {
 
+    Button btn_minus, btn_plus, btn_back;
+    TextView tv_jumlah_cart;
+    Integer valuejumlahcart = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        btn_minus = findViewById(R.id.btn_minus);
+        btn_plus = findViewById(R.id.btn_plus);
+        btn_back = findViewById(R.id.btn_back);
+        tv_jumlah_cart = findViewById(R.id.tv_jumlah_cart);
+
+        tv_jumlah_cart.setText(valuejumlahcart.toString());
+        // secara default, we hide btn_minus
+        btn_minus.animate().alpha(0).setDuration(300).start();
+        btn_minus.setEnabled(false);
+
+        btn_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuejumlahcart+=1;
+                tv_jumlah_cart.setText(valuejumlahcart.toString());
+                if (valuejumlahcart > 1) {
+                    btn_minus.animate().alpha(1).setDuration(300).start();
+                    btn_minus.setEnabled(true);
+                }
+            }
+        });
+
+        btn_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valuejumlahcart-=1;
+                tv_jumlah_cart.setText(valuejumlahcart.toString());
+                if (valuejumlahcart < 2) {
+                    btn_minus.animate().alpha(0).setDuration(300).start();
+                    btn_minus.setEnabled(false);
+                }
+            }
+        });
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backtohome = new Intent(CartActivity.this, MainActivity.class);
+                startActivity(backtohome);
+                finish();
+                overridePendingTransition(0,0);
+                getIntent().addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            }
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
