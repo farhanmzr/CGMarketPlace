363c5273166e4b9be55556d61e04d5a6fea5cbfa
Farhan Mazario <farhanrio2805@gmail.com>
2020-02-11 00:11:16 +0700

    upload ancok

--- app/src/main/java/com/example/cgmarketplace/activities/ConfirmPaymentActivity.java
+++ app/src/main/java/com/example/cgmarketplace/activities/ConfirmPaymentActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.Window;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,6 +34,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.storage.FirebaseStorage;
@@ -40,26 +42,30 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConfirmPaymentActivity extends AppCompatActivity {
 
-    private static final String TAG = "ConfirmedPaymentActivity";
+    private static final String TAG = "ConfirmPaymentActivity";
     private Uri filePath;
-    private final int PICK_IMAGE_REQUEST = 22;
+    private final int PICK_IMAGE_REQUEST = 5;
     private FirebaseUser user;
     private FirebaseFirestore mFirestore;
     private DocumentReference mUserRef, mPaymentRef;
     private FirebaseAuth mAuth;
     private Query mQuery;
     FirebaseStorage storage;
-    StorageReference storageReference;
+    private StorageReference mStorageRef;
 
     private Dialog alertDialog;
+    private EditText etOrderId;
     private TextView tvTitle, tvSub_title;
     private Button btn_cancel, btn_confirm;
     private ImageView imgUpload, changeImg_upload;
-    private String userId, orderId;
+    private String userId, orderId, imagesPayment;
 
     private Button btnUpload;
 
@@ -77,10 +83,10 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
         userId = mAuth.getCurrentUser().getUid();
         user = mAuth.getCurrentUser();
         storage = FirebaseStorage.getInstance();
-        storageReference = storage.getReference();
+        mStorageRef = storage.getReference();
         mUserRef = mFirestore.collection("Users").document(userId);
-        mPaymentRef = mFirestore.collection("Users").document(userId).collection("Payment").document(orderId);
-        mQuery = mFirestore.collection("Users").document(userId).collection("Orders").document(orderId).collection("paymentOrder");
+        mPaymentRef = mFirestore.collection("Users").document(userId).collection("Payments").document();
+
 
 
         tvTitle = findViewById(R.id.tvTitle);
@@ -108,6 +114,11 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
         tvSub_title = alertDialog.findViewById(R.id.tvSub_title);
         btn_cancel = alertDialog.findViewById(R.id.btn_cancel);
         btn_confirm = alertDialog.findViewById(R.id.btn_confirm);
+
+        etOrderId = findViewById(R.id.etOrderId);
+
+
+
         btn_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -119,21 +130,24 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent gotosuccess = new Intent(ConfirmPaymentActivity.this,SuccessUploadPaymentActivity.class);
                 alertDialog.dismiss();
+                UploadImg();
                 startActivity(gotosuccess);
                 finish();
             }
         });
 
         Glide.with(imgUpload.getContext())
-                .load(user.getPhotoUrl())
+                .load(getDrawable(R.drawable.img_default_payment))
                 .into(imgUpload);
 
         changeImg_upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                        selectImage();
+                    }
 
-                selectImage();
-                UploadImg();
+                });
+
             }
 
             private void selectImage() {
@@ -148,6 +162,46 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
                         PICK_IMAGE_REQUEST);
             }
 
+            @Override
+            protected void onActivityResult(int requestCode,
+                                            int resultCode,
+                                            Intent data) {
+
+                super.onActivityResult(requestCode,
+                        resultCode,
+                        data);
+
+                Uri ImageData = data.getData();
+
+
+                // checking request code and result code
+                // if request code is PICK_IMAGE_REQUEST and
+                // resultCode is RESULT_OK
+                // then set image in the image view
+                if (requestCode == PICK_IMAGE_REQUEST
+                        && resultCode == RESULT_OK
+                        && data != null
+                        && data.getData() != null) {
+
+                    // Get the Uri of data
+                    filePath = data.getData();
+                    try {
+
+                        // Setting image on image view using Bitmap
+                        Bitmap bitmap = MediaStore
+                                .Images
+                                .Media
+                                .getBitmap(
+                                        getContentResolver(),
+                                        filePath);
+                        imgUpload.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        // Log the exception
+                        e.printStackTrace();
+                    }
+                }
+            }
+
             public String GetFileExtension(Uri uri) {
 
                 ContentResolver contentResolver = getContentResolver();
@@ -159,24 +213,25 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
 
             }
 
-
             public void UploadImg() {
+
+
 
                 if (filePath != null) {
 
-                    // Code for showing progressDialog while uploading
                     final ProgressDialog progressDialog
-                            = new ProgressDialog(ConfirmPaymentActivity.this);
+                            = new ProgressDialog(this);
                     progressDialog.setTitle("Uploading...");
                     progressDialog.show();
-                    changeImg_upload.setVisibility(View.INVISIBLE);
+
+
 
                     // Defining the child of storageReference
                     final StorageReference ref
-                            = storageReference
+                            = mStorageRef
                             .child(
-                                    "imagesUser/"
-                                            + userId + "." + GetFileExtension(filePath));
+                                    "imagesPayments/"
+                                             + orderId + "." + GetFileExtension(filePath));
 
                     // adding listeners on upload
                     // or failure of image
@@ -186,40 +241,24 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
 
                                         @Override
                                         public void onSuccess(
-                                                UploadTask.TaskSnapshot taskSnapshot)
-                                        {
-                                            // Image uploaded successfully
-                                            // Dismiss dialog
-                                            progressDialog.dismiss();
+                                                UploadTask.TaskSnapshot taskSnapshot) {
 
                                             ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                                                 @Override
-                                                public void onSuccess(Uri uri) {
+                                                public void onSuccess(final Uri uri) {
 
-                                                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                                                    UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder().setPhotoUri(uri).build();
+                                                    orderId = etOrderId.getText().toString();
 
 
-                                                    user.updateProfile(profileUpdate)
-                                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                                class TAG {
-                                                                }
-
-                                                                @SuppressLint("LongLogTag")
-                                                                @Override
-                                                                public void onComplete(@NonNull Task<Void> task) {
-                                                                    Log.d(TAG, "Success Upload Payment");
-                                                                }
-                                                            });
                                                 }
                                             });
+
                                         }
                                     })
 
                             .addOnFailureListener(new OnFailureListener() {
                                 @Override
-                                public void onFailure(@NonNull Exception e)
-                                {
+                                public void onFailure(@NonNull Exception e) {
 
                                     // Error, Image not uploaded
                                     progressDialog.dismiss();
@@ -237,64 +276,19 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
                                         // percentage on the dialog box
                                         @Override
                                         public void onProgress(
-                                                UploadTask.TaskSnapshot taskSnapshot)
-                                        {
+                                                UploadTask.TaskSnapshot taskSnapshot) {
                                             double progress
                                                     = (100.0
                                                     * taskSnapshot.getBytesTransferred()
                                                     / taskSnapshot.getTotalByteCount());
                                             progressDialog.setMessage(
                                                     "Uploaded "
-                                                            + (int)progress + "%");
+                                                            + (int) progress + "%");
                                         }
                                     });
                 }
             }
 
-
-        });
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode,
-                                    int resultCode,
-                                    Intent data)
-    {
-
-        super.onActivityResult(requestCode,
-                resultCode,
-                data);
-
-        // checking request code and result code
-        // if request code is PICK_IMAGE_REQUEST and
-        // resultCode is RESULT_OK
-        // then set image in the image view
-        if (requestCode == PICK_IMAGE_REQUEST
-                && resultCode == RESULT_OK
-                && data != null
-                && data.getData() != null) {
-
-            // Get the Uri of data
-            filePath = data.getData();
-            try {
-
-                // Setting image on image view using Bitmap
-                Bitmap bitmap = MediaStore
-                        .Images
-                        .Media
-                        .getBitmap(
-                                getContentResolver(),
-                                filePath);
-                imgUpload.setImageBitmap(bitmap);
-            }
-
-            catch (IOException e) {
-                // Log the exception
-                e.printStackTrace();
-            }
-        }
-    }
 
     private void initViewComponents() {
         btnUpload = findViewById(R.id.btnUpload);
@@ -321,4 +315,5 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
